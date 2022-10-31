f35c815c25e43b1b06e412c2ffe3038f7d1bdfff
Alex Telon <alex.telon@gmail.com>
2022-10-31 20:35:37 +0100

    Cleaned up main.py

--- main.py
+++ main.py
@@ -1,64 +1,9 @@
 from PIL import Image, ImageOps, ImageMath, ImageFilter, ImageGrab
 import os
 import ImgFunctions as imf
-import FileFunctions as ff
 from pathlib import Path
 from typing import List, Callable
-from functools import partial
 from itertools import combinations, product, permutations
-import numpy as nm
-
-#                       Settings for the program
-#-------------------------------------------------------------------------
-
-# Make bitmaps from the bin files and store them in the input folder:
-# os.system('./Bitmapizer -bitmapize')
-
-
-# Define input and output directories and create subfolders for each colour depth:
-PATH_INPUT = Path('input')
-PATH_OUTPUT = Path('output')
-
-PATH_INPUT.mkdir(exist_ok=True)
-
-# clean up the output 
-for f in PATH_OUTPUT.glob('*.png'):
-    try:
-        os.remove(f)
-    except OSError as e:
-        print("Error: %s : %s" % (f, e.strerror))
-
-# def loop_through_function(im: Image.Image, steps: List[Callable[[Image.Image], Image.Image]]) -> Image.Image:
-#     for step in steps:
-#         im = step(im)
-#     return im
-
-#     # Looking at how to do manual tresholds in pillow.
-#     # multibands = im.split()
-#     # red      = multibands[0].point(lambda intensity: intensity)
-#     # green    = multibands[1].point(lambda intensity: intensity)
-#     # blue     = multibands[2].point(lambda intensity: intensity)
-#     # # red = ImageMath.eval("", a = red)
-#     # im = Image.merge("RGB", (red, green, blue))
-
-#     # Removed crop since my sample image does not need it
-#     # croppedIm = imf.cropImage(im, SQUARE)
-#     # greyIm = imf.makeGrayscale(croppedIm)
-#     # im = imf.reduce_quality_of_image(im, (100 - quality_percent))
-#     # im = imf.make_grayscale(im, bits)
-
-#     # Try to despeckle the image
-#     # im = im.filter(ImageFilter.BLUR)
-#     # im = im.filter(ImageFilter.MinFilter(3))
-#     # im = im.filter(ImageFilter.MinFilter)
-
-#     # Invert image
-#     # im = ImageOps.invert(im)
-
-#     # name = "_".join([Path(filename).stem, f'{bits}bit', f'{quality_percent}ppt']) + '.bmp'
-#     # new_path_output = path_output / f"{quality_percent}ppt"
-#     # ff.make_directory(new_path_output)
-#     # im.save(new_path_output / name)
 
 class Filter:
     def __init__(self, func, name, **kwargs):
@@ -68,7 +13,6 @@ class Filter:
 
     def __call__(self, im: Image.Image) -> Image.Image:
         return self.func(im, **self.kwargs)
-
 
 # All the quality reducing variants in a list.
 # quality_funcs = [Filter(imf.reduce_quality_of_image, f'{quality_percent}ppt', reduce_percentage=(100 - quality_percent)) for quality_percent in range(1, 20, 1)]
@@ -91,17 +35,8 @@ minfilter_funcs = [
     Filter(lambda im: im, '')
     ]
 
-options = [
-    quality_funcs,
-    greyscale_funs,
-    # blur_funcs,
-    invert_funcs,
-    minfilter_funcs,
-]
 
-def execute_steps_on_one_image(steps):
-    im = Image.open(PATH_INPUT / 'sample.bmp')
-
+def execute_steps_on_one_image(steps: List[Callable[[Image.Image], Image.Image]], im: Image.Image):
     # Run all steps.
     for func in steps:
         im = func(im)
@@ -110,14 +45,38 @@ def execute_steps_on_one_image(steps):
     im.save(PATH_OUTPUT / ('_'.join(f.name for f in steps) + '.png'))
 
 
-def execute_reordered_combinations_on_one_image(options):
-    # Run all combinations on one image
+def execute_reordered_combinations_on_one_image(options: List[List[Callable[[Image.Image], Image.Image]]], im: Image.Image):
     for steps in product(*options):
+        # Run all combinations on one image
         for comb in permutations(steps):
-            execute_steps_on_one_image(comb)
-            
+            execute_steps_on_one_image(comb, im)
+
+
 if __name__ == "__main__":
-    execute_reordered_combinations_on_one_image(options)
+    # Define input and output directories and create subfolders for each colour depth:
+    PATH_INPUT = Path('input')
+    PATH_OUTPUT = Path('output')
+
+    PATH_INPUT.mkdir(exist_ok=True)
+
+    # Clean up the output.
+    for f in PATH_OUTPUT.glob('*.png'):
+        try:
+            os.remove(f)
+        except OSError as e:
+            print("Error: %s : %s" % (f, e.strerror))
+
+
+    options = [
+        quality_funcs,
+        greyscale_funs,
+        # blur_funcs,
+        invert_funcs,
+        minfilter_funcs,
+    ]
+
+    im = Image.open(PATH_INPUT / 'sample.bmp')
+    execute_reordered_combinations_on_one_image(options, im)
     exit()
 
     # The square that the program will cut out and use
